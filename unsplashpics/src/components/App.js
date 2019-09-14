7aac80cca48eb634dc343e5a816df5fb5c06364a
TNORTHERN <traynorthern@yahoo.com>
2019-09-14 10:15:03 -0500

    Adding Semantic

--- unsplashpics/src/components/App.js
+++ unsplashpics/src/components/App.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import SearchBar from './SearchBar'
 
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <SearchBar/>
+        </div>
     );
 }
 
